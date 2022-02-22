30525c8402bdf5133c3918762394836700651752
Bogdan Rata <bogdan.rata1289@gmail.com>
2022-02-22 18:46:50 +0200

    Add QuizQuestionsActivity + data model

--- MyQuizApp/app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.kt
+++ MyQuizApp/app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.kt
@@ -2,10 +2,18 @@ package com.example.myquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        val questionList = Constant.getQuestions()
+        Log.e("Question Size", "${questionList.size}")
+
+        for (i in questionList) {
+            Log.e("Question", i.question)
+        }
     }
 }
