ae2adb5a89bdb54d00f5bbd59472d1c28191bc8c
Bogdan Rata <bogdan.rata1289@gmail.com>
2022-02-22 19:48:38 +0200

    Add QuizQuestionsActivity add on click event.

--- MyQuizApp/app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.kt
+++ MyQuizApp/app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.kt
@@ -1,13 +1,22 @@
 package com.example.myquizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+    private var mCurrentPosition: Int = 1
+    private var mQuestionList: MutableList<Question>? = null
+
+    private var mSelectedOptionPosition: Int = 0
+
     private var progressBar: ProgressBar? = null
     private var tvProgress: TextView? = null
     private var tvQuestion: TextView? = null
@@ -21,11 +30,9 @@ class QuizQuestionsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        val questionList = Constant.getQuestions()
-        Log.e("Question Size", "${questionList.size}")
-        val currentPosition = 1
+        mQuestionList = Constant.getQuestions()
+        Log.e("Question Size", "${mQuestionList!!.size}")
 
-        val question: Question? = questionList[currentPosition - 1]
         progressBar = findViewById(R.id.progressBar)
         tvProgress = findViewById(R.id.tv_progress)
         tvQuestion = findViewById(R.id.tv_question)
@@ -34,15 +41,91 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tvOptionTwo = findViewById(R.id.tv_option_two)
         tvOptionThree = findViewById(R.id.tv_option_three)
         tvOptionFour = findViewById(R.id.tv_option_four)
+        setQuestion()
 
-        progressBar?.progress = currentPosition
-        tvProgress?.text = "$currentPosition/${progressBar?.max}"
+        tvOptionOne?.setOnClickListener(this)
+        tvOptionTwo?.setOnClickListener(this)
+        tvOptionThree?.setOnClickListener(this)
+        tvOptionFour?.setOnClickListener(this)
+    }
 
-        tvQuestion?.text = question!!.question
+
+    private fun setQuestion() {
+        val question: Question = mQuestionList!![mCurrentPosition - 1]
+
+        progressBar?.progress = mCurrentPosition
+        tvProgress?.text = "$mCurrentPosition/${progressBar?.max}"
+
+        tvQuestion?.text = question.question
         ivImage?.setImageResource(question.image)
         tvOptionOne?.text = question.optionOne
         tvOptionTwo?.text = question.optionTwo
         tvOptionThree?.text = question.optionThree
         tvOptionFour?.text = question.optionFour
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.tv_option_one -> {
+                tvOptionOne?.let {
+                    selectedOption(it, 1)
+                }
+            }
+            R.id.tv_option_two -> {
+                tvOptionTwo?.let {
+                    selectedOption(it, 2)
+                }
+            }
+            R.id.tv_option_three -> {
+                tvOptionThree?.let {
+                    selectedOption(it, 3)
+                }
+            }
+            R.id.tv_option_four -> {
+                tvOptionFour?.let {
+                    selectedOption(it, 4)
+                }
+            }
+        }
+    }
+
+    private fun selectedOption(tv: TextView, selectedOptionNum: Int) {
+        defaultOptionView()
+
+        mSelectedOptionPosition = selectedOptionNum
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this, R.drawable.selected_option_border_bg
+        )
+    }
+
+    private fun defaultOptionView() {
+
+        val options = ArrayList<TextView>()
+
+        tvOptionOne?.let {
+            options.add(0, it)
+        }
+
+        tvOptionTwo?.let {
+            options.add(1, it)
+        }
+        tvOptionThree?.let {
+            options.add(2, it)
+        }
+        tvOptionFour?.let {
+            options.add(3, it)
+        }
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this, R.drawable.default_option_border_bg
+            )
+        }
+
+
     }
 }
